a9d0087c00c60ec4410ad44a7949b22b15c126c5
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-08-10 18:26:03 -0400

    loops

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -215,7 +215,29 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-	console.log(battleLog);
+	for (let i = 0; i < 3; i++) {
+		console.log("------------");
+	}
+	let j = 3;
+	do {
+		console.log(j);
+		j++;
+	} while (j < 3);
+	// for (let i = 10; i > 0;) {
+	//   i--;
+	//   console.log(i);
+	// }
+	// for (let i = 0; i < battleLog.length; i++) {
+	//   console.log(battleLog[i]);
+	// }
+	let i = 0;
+	for (const logEntry of battleLog) {
+		console.log(`#${i}`);
+		for (const key in logEntry) {
+			console.log(`${key} => ${logEntry[key]}`);
+		}
+		i++;
+	}
 }
 
 attackBtn.addEventListener("click", attackHandler);
